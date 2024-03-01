fc652f7b791bd3d645e60465992e8a9d2db4bd6e
Harish Samtiya <95499754+harishsamtiya@users.noreply.github.com>
2024-03-01 15:43:13 +0930

    Time: 41 ms (39.01%) | Memory: 16.5 MB (96.12%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -7,7 +7,5 @@ class Solution:
             if ch == '1':
                 oneCount += 1
 
-        if oneCount:   
-            return "1"*(oneCount-1) + "0"*(n-oneCount) + "1"*(1)
+        return "1"*(oneCount-1) + "0"*(n-oneCount) + "1"*(1)
 
-        return s
