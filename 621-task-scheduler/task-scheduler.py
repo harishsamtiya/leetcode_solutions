7c89797b9247194b04daa30efaf83d7c863670ec
Harish Samtiya <95499754+harishsamtiya@users.noreply.github.com>
2024-03-19 09:58:27 +0530

    Time: 421 ms (66.04%) | Memory: 17.1 MB (59.31%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,32 +1,37 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        if n == 0:
-            return len(tasks)
-        chars = [0]*26
+        ch_array = [0]*26
 
         for ch in tasks:
-            ind = ord(ch) - 65
-            chars[ind] += 1
-        
+            ind = ord(ch)-65
+            ch_array[ind] += 1
+
         pq = []
-        for val in chars:
-            if val:
-                pq.append(-val)
-    
+
+        for freq in ch_array:
+            if freq > 0:
+                pq.append(-freq)
+        
         heapify(pq)
         time = 0
         queue = []
+
         while pq or queue:
-            time += 1
             if pq:
                 val = heappop(pq)
-                val += 1
-                if val:
-                    queue.append((time+n, val))
-            if queue and queue[0][0] <= time:
+            else:
+                tillTime, val = queue.pop(0)
+                time = tillTime + 1
+
+            val += 1
+
+            if val != 0:
+                queue.append((time+n, val))
+            
+            if queue and queue[0][0] == time:
                 tillTime, val = queue.pop(0)
                 heappush(pq, val)
-
-
-        return time
+            time += 1
             
+        return time
+
