0d982e55ada6d58c391428ef085721a946fd3712
Harish Samtiya <95499754+harishsamtiya@users.noreply.github.com>
2024-04-12 12:07:51 +0530

    Time: 115 ms (23.58%) | Memory: 18.5 MB (39.10%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,19 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        l, r = 0, n-1
-
-        vol = 0
-        sea_level = 0
-
-        while l < r:
-
-            if height[l] <= sea_level:
-                vol += sea_level - height[l]
-                l += 1
-            elif height[r] <= sea_level:
-                vol += sea_level - height[r]
-                r -= 1
+        maxHeight = 0
+        i, j = 0, len(height)-1
+        trapWater = 0
+        
+        while i < j:
+            maxHeight = max(maxHeight, min(height[i], height[j]))
+            if height[i] > height[j]:
+                trapWater += maxHeight - min(maxHeight, height[j])
+                j -= 1
             else:
-                sea_level = min(height[l], height[r])
-        return vol
+                trapWater += maxHeight - min(maxHeight, height[i])
+                i += 1
+
+        return trapWater
