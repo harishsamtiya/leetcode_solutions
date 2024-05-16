16746888cd1735d7909414560b281323ccc92ff3
Harish Samtiya <95499754+harishsamtiya@users.noreply.github.com>
2024-05-16 11:16:55 +0530

    Time: 52 ms (23.68%) | Memory: 16.8 MB (49.94%) - LeetSync

---
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(node):
+            if node.val == 0:
+                return False
+            elif node.val == 1:
+                return True
+            elif node.val == 2:
+                return dfs(node.left) or dfs(node.right)
+            else:
+                return dfs(node.left) and dfs(node.right)
+        
+        return dfs(root)
