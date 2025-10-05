1abd618b1c3f6aa995f2e4f6785ac09d1c85fbe0
Harish Samtiya <95499754+harishsamtiya@users.noreply.github.com>
2025-10-05 22:23:57 +0530

    Time: 23 ms (93.84%) | Memory: 19.2 MB (79.71%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,37 +1,37 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         n, m = len(heights), len(heights[0])
-        flow_ocean = [[True]*m for _ in range(n)]
+        oceans = [[[False]*m for _ in range(n)], [[False]*m for _ in range(n)]]
         cols = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        def dfs(i, j):
-            flow_ocean[i][j] = False
-            pacific, atlantic = False, False
-            for r, c in cols:
-                row = i + r
-                col = j + c
-                if pacific and atlantic:
-                    break
-                elif ((row == 0 and col == m-1) or (col == 0 and row == n-1)) and heights[i][j] >= heights[row][col]:
-                    pacific = True
-                    atlantic =  True
-                elif row < 0 or col < 0:
-                    pacific = True
-                elif row >= n or col >= m:
-                    atlantic =  True
-                elif flow_ocean[row][col] and heights[i][j] >= heights[row][col]:
-                    pac, atlan = dfs(row, col)
-                    pacific = pacific or pac
-                    atlantic = atlantic or atlan
-                    
-            flow_ocean[i][j] = True
-            return (pacific, atlantic)
-
         result = []
+        stack = []
+
+        for j in range(m):
+            oceans[0][0][j] = True
+            oceans[1][n-1][j] = True
+            stack.append((0, 0, j))
+            stack.append((1, n-1, j))
         for i in range(n):
-            for j in range(m):
-                pac, atlan = dfs(i, j)
-                if pac and atlan:
-                    result.append([i, j])
+            oceans[0][i][0] = True
+            oceans[1][i][m-1] = True
+            stack.append((0, i, 0))
+            stack.append((1, i, m-1))
+
+        while stack:
+            curr_ocean, i, j = stack.pop()
+
+            for r, c in cols:
+                row, col = i + r, j + c
+                if 0 <= row < n and 0 <= col < m and (not oceans[curr_ocean][row][col]) and heights[i][j] <= heights[row][col]:
+                    oceans[curr_ocean][row][col] = True
+                    stack.append((curr_ocean, row, col))
+        
+
+        for row in range(n):
+            for col in range(m):
+                if oceans[0][row][col] and oceans[1][row][col]:
+                    result.append([row, col])
         return result
-                    
+
+        
+        
